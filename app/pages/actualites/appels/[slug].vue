d7a3ba76af4f98368aeebb3c85236ef05cf73d4e
Angenor <angenor99@gmail.com>
2026-02-10 16:06:52 +0000

    ok

--- app/pages/actualites/appels/[slug].vue
+++ app/pages/actualites/appels/[slug].vue
@@ -147,7 +147,7 @@ useSeoMeta({
     />
 
     <!-- Tabs Navigation -->
-    <CallsCallTabs :active-tab="activeTab" />
+    <!-- <CallsCallTabs :active-tab="activeTab" /> -->
 
     <!-- Content -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
